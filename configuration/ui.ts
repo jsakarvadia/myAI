9fbd6d195556c9db049edc7e778b6d136b0ae5fd
Jay Sakarvadia <107783145+jsakarvadia@users.noreply.github.com>
2025-02-27 22:52:39 -0500

    Update ui.ts
    changed to JayGPT

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `JayBot`;
+export const CHAT_HEADER: string = `JayGPT`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = ``;
 export const CLEAR_BUTTON_TEXT: string = `Reset Chat`;
-export const PAGE_TITLE: string = `JayBot`;
+export const PAGE_TITLE: string = `JayGPT`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
